1373422bad427cac39226e7ed6f2d3afac78b6dc
retr0mouse <daniil.sharin667@gmail.com>
2026-07-01 21:40:52 +0300

    add nginx, immich

--- system/hosts/nico/configuration.nix
+++ system/hosts/nico/configuration.nix
@@ -13,9 +13,103 @@
   networking.networkmanager.enable = true;
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 53 80 ]; # port 22 opened automatically by services.openssh
+    allowedTCPPorts = [ 53 80 2283 ]; # port 22 opened automatically by services.openssh
     allowedUDPPorts = [ 53 ];
   };
+
+  services.nginx = {
+    enable = true;
+
+    virtualHosts."nico.immich" = {
+      listen = [
+        { addr = "0.0.0.0"; port = 80; }
+      ];
+
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:2283";
+
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
+      };
+    };
+
+    virtualHosts."immich.local" = {
+      listen = [
+        { addr = "0.0.0.0"; port = 8080; }
+      ];
+
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:2283";
+      };
+    };
+
+    virtualHosts."nico.pihole" = {
+      listen = [
+        { addr = "0.0.0.0"; port = 80; }
+      ];
+
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8081";
+
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
+      };
+    };
+
+    recommendedProxySettings = true;
+    recommendedGzipSettings = true;
+  };
+
+
+  fileSystems."/data" = {
+    device = "/dev/disk/by-uuid/efee3c35-c283-4091-9a72-5df4cfcb2412";
+    fsType = "ext4";
+    options = [ "noatime" "nofail" "x-systemd.device-timeout=5s" ];
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /data/immich 0750 immich immich -"
+    "d /data/immich/library 0750 immich immich -"
+    "d /data/immich/upload 0750 immich immich -"
+    "d /data/immich/thumbs 0750 immich immich -"
+    "d /data/immich/postgres 0700 postgres postgres -"
+  ];
+
+  services.postgresql = {
+    enable = true;
+    dataDir = "/data/postgres/immich";
+
+    authentication = pkgs.lib.mkOverride 10 ''
+      local all all trust
+      host all all 127.0.0.1/32 trust
+      host all all ::1/128 trust
+    '';
+  };
+
+  services.immich = {
+    enable = true;
+
+    host = "127.0.0.1";
+    port = 2283;
+
+    mediaLocation = "/data/immich/library";
+  };
+  
+  users.users.immich = {
+    isSystemUser = true;
+    group = "immich";
+    home = "/var/lib/immich";
+  };
+
+  users.groups.immich = {};
 
   services.logind = {
     lidSwitch = "ignore";
@@ -47,7 +141,7 @@
 
   services.pihole-web = {
     enable = true;
-    ports = [ "80" ];
+    ports = [ "8081" ];
   };
 
   services.pihole-ftl = {
@@ -55,6 +149,10 @@
     settings = {
       dns.interface = "0.0.0.0";
       dns.upstreams = [ "127.0.0.1#5335" ];
+      dns.hosts = [
+        "192.168.0.251 nico.immich"
+        "192.168.0.251 nico.pihole"
+      ];
 
       adlists = [
         "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
